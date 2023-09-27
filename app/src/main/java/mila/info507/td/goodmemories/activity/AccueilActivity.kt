ab6167fdae705709da84381f536da24389379a02
Ameliorer <ameliehacque@gmail.com>
2023-09-27 16:00:53 +0200

    Revert "files"
    This reverts commit 4fca130b873ed1dc4c43a8308f3b1a6914d26b5b.

--- app/src/main/java/mila/info507/td/goodmemories/activity/AccueilActivity.kt
+++ app/src/main/java/mila/info507/td/goodmemories/activity/AccueilActivity.kt
@@ -1,12 +1,12 @@
-package activity
+package mila.info507.td.goodmemories.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mila.info507.td.goodmemories.R
 
-class MainActivity : AppCompatActivity() {
+class AccueilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_accueil)
     }
 }
