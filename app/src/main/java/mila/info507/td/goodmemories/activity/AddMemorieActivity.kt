57fefe6c5535f68068c82ab1544743a3de7005f9
Mila Desmet <desmetmila@gmail.com>
2023-10-16 20:35:46 +0200

    Page d'ajout de memorie

--- app/src/main/java/mila/info507/td/goodmemories/activity/AddMemorieActivity.kt
+++ app/src/main/java/mila/info507/td/goodmemories/activity/AddMemorieActivity.kt
@@ -1,12 +1,71 @@
 package mila.info507.td.goodmemories.activity
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import mila.info507.td.goodmemories.R
+import java.util.Calendar
 
 class AddMemorieActivity : AppCompatActivity() {
+
+    lateinit var dateEdt : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_memorie)
+
+        // Récuperation de la liste des émotions
+
+        //val rem : Request
+        //val emotions :
+
+
+
+        //-------------------------------
+        // GESTION DE L'INPUT DATE
+        //-------------------------------
+        // code from : https://www.geeksforgeeks.org/how-to-popup-datepicker-while-clicking-on-edittext-in-android/
+        // on below line we are initializing our variables.
+        dateEdt = findViewById(R.id.input_date)
+
+        // on below line we are adding
+        // click listener for our edit text.
+        dateEdt.setOnClickListener {
+
+            // on below line we are getting
+            // the instance of our calendar.
+            val c = Calendar.getInstance()
+
+            // on below line we are getting
+            // our day, month and year.
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            // on below line we are creating a
+            // variable for date picker dialog.
+            val datePickerDialog = DatePickerDialog(
+                // on below line we are passing context.
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    // on below line we are setting
+                    // date to our edit text.
+                    val dat = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                    dateEdt.setText(dat)
+                },
+                // on below line we are passing year, month
+                // and day for the selected date in our date picker.
+                year,
+                month,
+                day
+            )
+            // at last we are calling show
+            // to display our date picker dialog.
+            datePickerDialog.show()
+        }
     }
+
+
+
+
 }
